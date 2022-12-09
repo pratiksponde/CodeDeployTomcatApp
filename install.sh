4730954e28530065e14adabf2c3f7f0df0218241
pratiksponde <85301766+pratiksponde@users.noreply.github.com>
2022-12-09 15:21:11 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-aws s3 cp s3://bucketforjenkins11111/target/LoginWebApp-1.war/sample-java-projects/LoginWebApp-1.war /tmp
+aws s3 cp s3://bucketforjenkins11111/Sample/LoginWebApp-1.war /tmp
 sudo mv /tmp/LoginWebApp-1.war /usr/share/tomcat/webapps/LoginWebApp-1.war
 sudo service tomcat restart
